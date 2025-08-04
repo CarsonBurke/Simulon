c0f468de313f2896a6305e61611c8694f4c75132
Carson <carsonburke22@gmail.com>
2025-08-03 19:59:43 -0700

    basic laws

---
+++ src/constants/laws.rs
@@ -0,0 +1,6 @@
+/// Employees must be employed for at least 14 days
+pub const MIN_EMPLOYMENT_DAYS: u32 = 14;
+/// Employees must be given at least 7 days notice before being fired to look for new employment
+pub const FIRING_NOTICE_DAYS: u32 = 7;
+/// Nobody can make more than the bottom 10%?
+pub const MIN_WAGE_PERCENTILE: u32 = 10;
