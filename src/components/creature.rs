ae86fc02d970e983b22aa3acb4508653c178ce8b
Carson <carsonburke22@gmail.com>
2025-08-03 21:11:57 -0700

    creature heritables

--- src/components/creature.rs
+++ src/components/creature.rs
@@ -1,4 +1,7 @@
 use bevy::prelude::*;
+use rand::Rng;
+
+use crate::constants::creature::heritable_initial_ranges;
 
 #[derive(Component)]
 pub struct Creature {
@@ -9,25 +12,36 @@ pub struct Creature {
     pub knowledge: f32,
     pub intelligence: f32,
     pub charisma: f32,
+    pub kind: CreatureKind,
 }
 
 impl Creature {
+    pub fn new(kind: CreatureKind) -> Self {
+        let heritable = CreatureHeritable::new_random();
+        Self {
+            health: heritable.max_health,
+            heritable,
+            age: 0,
+            maturity: 0.0,
+            knowledge: 0.0,
+            intelligence: 0.0,
+            charisma: 0.0,
+            kind,
+        }
+    }
+
     pub fn attractiveness(&self) -> f32 {
-        self.intelligence
-            + self.charisma
-            + self.knowledge
-            + self.heritable.children_desire
+        self.intelligence + self.charisma + self.knowledge + self.heritable.children_desire
     }
 }
 
+#[derive(Clone)]
 pub struct CreatureHeritable {
     pub kind: CreatureKind,
-    /**
-     * For every increase in age, increase the chance of death by this amount.
-     */
+    /// For every increase in age, increase the chance of death by this amount.
     pub age_death_chance: f32,
+    pub max_health: f32,
     pub maturity_rate: f32,
-    pub age_rate: f32,
     pub aggression: f32,
     pub intelligence: f32,
     pub charisma: f32,
@@ -37,6 +51,52 @@ pub struct CreatureHeritable {
     pub children_desire: f32,
 }
 
+impl CreatureHeritable {
+    pub fn new_random() -> Self {
+        let mut rng = rand::rng();
+
+        Self {
+            kind: CreatureKind::Human,
+            age_death_chance: rng.random_range(
+                heritable_initial_ranges::AGE_DEATH_CHANCE.0
+                    ..heritable_initial_ranges::AGE_DEATH_CHANCE.1,
+            ),
+            max_health: rng.random_range(
+                heritable_initial_ranges::MAX_HEALTH.0..heritable_initial_ranges::MAX_HEALTH.1,
+            ),
+            maturity_rate: rng.random_range(
+                heritable_initial_ranges::MATURITY_RATE.0
+                    ..heritable_initial_ranges::MATURITY_RATE.1,
+            ),
+            aggression: rng.random_range(
+                heritable_initial_ranges::AGGRESSION.0..heritable_initial_ranges::AGGRESSION.1,
+            ),
+            intelligence: rng.random_range(
+                heritable_initial_ranges::INTELLIGENCE.0..heritable_initial_ranges::INTELLIGENCE.1,
+            ),
+            charisma: rng.random_range(
+                heritable_initial_ranges::CHARISMA.0..heritable_initial_ranges::CHARISMA.1,
+            ),
+            material_desire: rng.random_range(
+                heritable_initial_ranges::MATERIAL_DESIRE.0
+                    ..heritable_initial_ranges::MATERIAL_DESIRE.1,
+            ),
+            social_desire: rng.random_range(
+                heritable_initial_ranges::SOCIAL_DESIRE.0
+                    ..heritable_initial_ranges::SOCIAL_DESIRE.1,
+            ),
+            food_desire: rng.random_range(
+                heritable_initial_ranges::FOOD_DESIRE.0..heritable_initial_ranges::FOOD_DESIRE.1,
+            ),
+            children_desire: rng.random_range(
+                heritable_initial_ranges::CHILDREN_DESIRE.0
+                    ..heritable_initial_ranges::CHILDREN_DESIRE.1,
+            ),
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
 pub enum CreatureKind {
     Human,
     Sheep,
--- src/constants/creature.rs
+++ src/constants/creature.rs
@@ -1,1 +1,29 @@
 pub const MAX_MATURITY: f32 = 100.0;
+
+pub mod heritable_initial_ranges {
+    pub const MAX_HEALTH: (f32, f32) = (10.0, 20.0);
+    pub const AGE_DEATH_CHANCE: (f32, f32) = (0.01, 1.0);
+    pub const MATURITY_RATE: (f32, f32) = (0.1, 1.0);
+    pub const MATURITY_SPEED: (f32, f32) = (0.1, 1.0);
+    pub const CHARISMA: (f32, f32) = (0.1, 1.0);
+    pub const INTELLIGENCE: (f32, f32) = (0.1, 1.0);
+    pub const AGGRESSION: (f32, f32) = (0.1, 1.0);
+    pub const MATERIAL_DESIRE: (f32, f32) = (0.1, 1.0);
+    pub const SOCIAL_DESIRE: (f32, f32) = (0.1, 1.0);
+    pub const FOOD_DESIRE: (f32, f32) = (0.1, 1.0);
+    pub const CHILDREN_DESIRE: (f32, f32) = (0.1, 1.0);
+}
+
+pub mod heritable_bounds {
+    pub const MAX_HEALTH: (f32, f32) = (2.0, 200.0);
+    pub const AGE_DEATH_CHANCE: (f32, f32) = (0.001, 5.0);
+    pub const MATURITY_RATE: (f32, f32) = (0.01, 10.0);
+    pub const MATURITY_SPEED: (f32, f32) = (0.01, 10.0);
+    pub const CHARISMA: (f32, f32) = (0.01, 10.0);
+    pub const INTELLIGENCE: (f32, f32) = (0.01, 10.0);
+    pub const AGGRESSION: (f32, f32) = (0.01, 10.0);
+    pub const MATERIAL_DESIRE: (f32, f32) = (0.01, 10.0);
+    pub const SOCIAL_DESIRE: (f32, f32) = (0.01, 10.0);
+    pub const FOOD_DESIRE: (f32, f32) = (0.01, 10.0);
+    pub const CHILDREN_DESIRE: (f32, f32) = (0.01, 10.0);
+}
